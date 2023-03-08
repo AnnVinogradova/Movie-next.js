96fad4ff07265574fea3545e0836d8c2e2817a6c
AnnVinogradova <saikotakaki@gmail.com>
2023-03-08 16:04:21 +0300

    Готово

--- component/Movieup.js
+++ component/Movieup.js
@@ -6,9 +6,12 @@ export default function Movieup({ selected, closeMovieup }) {
 		<section className='movieup'>
 			<div className='content'>
 				<h2>{selected.Title} <span>({selected.Year})</span></h2>
+				<span>({selected.Runtime})</span>
+				<span>({selected.Genre})</span>
 				<p className='rating'>Rating:{selected.imdbRating}</p>
 				<div className='plot'>
 					<img src={selected.Poster}></img>
+					<p>{selected.Actors}</p>
 					<p>{selected.Plot}</p>
 				</div>
 				<button className='close' onClick={closeMovieup}>Закрыть</button>
